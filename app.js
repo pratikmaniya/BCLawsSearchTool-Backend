6e6db7a60ab63418ade19546718c67891d6ae354
Pratik Maniya <pratikmmaniya244@gmail.com>
2024-10-08 13:41:29 -0400

    allowed CORS

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const express = require('express');
-const CORS = require('cors')
 const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(CORS())
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET');
+  next();
+});
 
 app.use('/', routes);
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
