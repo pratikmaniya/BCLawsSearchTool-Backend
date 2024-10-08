9685f34fcd8364af76941e7ff859a620ba4b44c1
Pratik Maniya <pratikmmaniya244@gmail.com>
2024-10-08 14:03:27 -0400

    added origin to cors

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require('express');
+const CORS = require('cors')
 const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(CORS())
 app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.setHeader('Access-Control-Allow-Origin', 'https://pratikmaniya.github.io');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
   next();
 });
 
