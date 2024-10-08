a2326a222ca46301ff12614ba68bc5a8bbc6ba5c
Pratik Maniya <pratikmmaniya244@gmail.com>
2024-10-08 14:00:32 -0400

    removed cors

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 const express = require('express');
-const CORS = require('cors')
 const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(CORS())
 app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
   next();
 });
 
